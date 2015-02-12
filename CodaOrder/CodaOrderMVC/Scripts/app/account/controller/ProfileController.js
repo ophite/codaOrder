02c12f968237aeef4aa9a97396084c23267ff577
Yura <ophite@ukr.net>
2015-02-12 19:56:12 +0200

    profile

--- CodaOrder/CodaOrderMVC/Scripts/app/account/controller/ProfileController.js
+++ CodaOrder/CodaOrderMVC/Scripts/app/account/controller/ProfileController.js
@@ -58,16 +58,9 @@
                 console.log(newValues, oldValues);
             }, true);
 
-            //         $scope.tabs = [
-            //{ title: 'Dynamic Title 1', content: 'Dynamic content 1' },
-            //{ title: 'Dynamic Title 2', content: 'Dynamic content 2', disabled: true }
-            //         ];
-
-            //         $scope.alertMe = function () {
-            //             setTimeout(function () {
-            //                 $window.alert('You\'ve selected the alert tab!');
-            //             });
-            //         };
+            $scope.save = function () {
+                console.log('save');
+            };
         }
     ]);
 })();
