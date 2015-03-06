a27a324344d2b1d41cd61c1060d41a7d9e0650c2
Yura <ophite@ukr.net>
2015-03-06 17:29:21 +0200

    test directive datepicker

--- CodaOrder/CodaOrderMVC/Scripts/app/test/karma.conf.js
+++ CodaOrder/CodaOrderMVC/Scripts/app/test/karma.conf.js
@@ -35,9 +35,25 @@ module.exports = function (config) {
             'Scripts/app/**/*.js',
             'Scripts/app/**/**/*.js',
             'Scripts/app/test/unit/**/*Spec.js',
-            'Scripts/app/document/controller/DocumentGridController.js'
+            'Scripts/app/view/*.html'
         ],
 
+        //ngHtml2JsPreprocessor: {
+        //    // strip this from the file path
+        //    stripPrefix: 'Scripts/'
+        //    //stripSufix: '.ext',
+        //    //// prepend this to the
+        //    //prependPrefix: 'served/',
+        //    //
+        //    //// or define a custom transform function
+        //    //cacheIdFromPath: function(filepath) {
+        //    //    return cacheId;
+        //    //},
+        //
+        //    // setting this option will create only a single module that contains templates
+        //    // from all the files, so you can load them all with module('foo')
+        //    //moduleName: 'preprocessTemplate'
+        //},
 
         // list of files to exclude
         exclude: [],
@@ -45,7 +61,9 @@ module.exports = function (config) {
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {},
+        preprocessors: {
+            'Scripts/**/**/*.html': ['ng-html2js']
+        },
 
 
         // test results reporter to use
@@ -57,9 +75,10 @@ module.exports = function (config) {
         // web server port
         port: 9876,
 
-        plugins : [
+        plugins: [
             'karma-chrome-launcher',
-            'karma-jasmine'
+            'karma-jasmine',
+            'karma-ng-html2js-preprocessor'
         ],
 
         // enable / disable colors in the output (reporters and logs)
@@ -68,7 +87,7 @@ module.exports = function (config) {
 
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
+        logLevel: config.LOG_INFO || config.LOG_DEBUG,
 
 
         // enable / disable watching file and executing tests whenever any file changes
--- CodaOrder/CodaOrderMVC/Scripts/app/test/unit/document/directiveSpec.js
+++ CodaOrder/CodaOrderMVC/Scripts/app/test/unit/document/directiveSpec.js
@@ -1,14 +1,52 @@
 describe('directive: document', function () {
-    var $compile, $rootScope, $httpBackend, $state;
+    var $compile, $rootScope, $httpBackend, $filter, $scope;
 
     beforeEach(module('app'));
-    beforeEach(inject(function (_$compile_, _$rootScope_, _$httpBackend_, _$state_) {
+    beforeEach(module('Scripts/app/view/DirDatePicker2.html'));
+
+    beforeEach(inject(function (_$compile_, _$rootScope_, _$httpBackend_, _$filter_) {
         $compile = _$compile_;
         $rootScope = _$rootScope_;
         $httpBackend = _$httpBackend_;
-        $state = _$state_;
+        $filter = _$filter_;
+        $scope = $rootScope.$new();
     }))
 
+    describe('test.dirDatePicker', function () {
+        it('should test dir date picker', function () {
+            //
+            $scope.format = 'dd.MM.yyyy';
+            $scope.dates = {};
+            var date = new Date();
+            $scope.dates.dateStart = formatDate(date);
+            date.setDate(date.getDate() + 7);
+            $scope.dates.dateEnd = formatDate(date);
+
+            function formatDate(dateRow) {
+                return $filter('date')(dateRow, $scope.format);
+            }
+
+            var element = angular.element('<div><dir-date-picker ng-model="dates.dateStart" date-format="{{format}}" /></div>');
+            $compile(element)($scope);
+            $scope.$digest();
+            var childIsolateScope = element.children().isolateScope();
+            var input = element.find('input');
+
+            expect(childIsolateScope).toBeDefined();
+            expect(input).toBeDefined();
+            //    expect(element.html()).toContain('min-date');
+            //    expect(element.html()).toContain('max-date');
+            //    expect(element.html()).toContain('datepicker-popup');
+            //    expect(element.html()).toContain('datepicker-options');
+        });
+    });
+
+    //describe('test.dirDatesValidation', function () {
+    //    it('should test dir dates validation', function () {
+    //
+    //    });
+    //});
+    //
     describe('test.dirLeftMenuLinks', function () {
         it('should be true', function () {
             // fire run request by all state's templateUrl and if not setting up url then get Error
